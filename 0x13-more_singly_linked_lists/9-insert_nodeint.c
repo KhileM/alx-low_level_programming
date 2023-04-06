3ae5328d42c10c6af30279d25f4e5c17602a8906
KhileM <Sakhile.motha.254@gmail.com>
2023-04-06 05:10:55 -0700

    A function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,64 +4,65 @@
 #include <string.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node to a listint_t
- *                           list at a given position.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @idx: The index of the listint_t list where the new
- *       node should be added - indices start at 0.
+ * insert_nodeint_at_index - Inserts a new node to a listint_t list
+ *                                at a given position.
+ *
+ * @head: A pointer to the address of the head of the listint_t list.
+ *
+ * @idx: The index of the listint_t list where the new node should be
+ *                    added - index starts at 0.
+ *
  * @n: The integer for the new node to contain.
  *
  * Return: If the function fails - NULL.
- *         Otherwise - the address of the new node.
+ *           Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *current_node;
-	unsigned int i;
+	unsigned int j;
 
-	/* Check if head is null or index is out of bounds */
 	if (head == NULL || idx > listint_len(*head))
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+			if (new_node == NULL)
+			return (NULL);
 
-	new_node->n = n;
+		new_node->n = n;
 
-	/* Insert at head */
-	if (idx == 0)
-	{
+		if (idx == 0)
+
+		{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
-	}
+		}
 
-	/* Insert at other positions */
-	current_node = *head;
-	for (i = 0; i < idx - 1; i++)
+		current_node = *head;
+		for (j = 0; j < idx - 1, j++;)
 		current_node = current_node->next;
 
-	new_node->next = current_node->next;
-	current_node->next = new_node;
+		new_node->next = current_node->next;
+		current_node->next = new_node;
 
-	return (new_node);
+		return (new_node);
 }
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list.
+ * listint_len - Returns the number of elements in a linted listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
+ *
  */
-size_t listint_len(const listint_t *h)
+size_t listint_len(count listint_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		count++;
+		count++
 		h = h->next;
 	}
 
